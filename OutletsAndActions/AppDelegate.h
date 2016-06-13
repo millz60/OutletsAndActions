c932f192ab23e6a68950808420f2c2b487a8ed46
millz60 <Matt@matts-macbook-pro.local>
2016-06-13 14:18:59 -0500

    Initial Commit

---
+++ OutletsAndActions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutletsAndActions
+//
+//  Created by Matt Milner on 6/13/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OutletsAndActions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutletsAndActions
+//
+//  Created by Matt Milner on 6/13/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
